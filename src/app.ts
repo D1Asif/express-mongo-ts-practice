57aa271f15922990780de15bf07de52e97142e70
Dewan Asifur Rahman <dasifrahman@gmail.com>
2024-06-07 21:35:26 +0600

    final commit

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { StudentRoutes } from "./app/modules/student/student.route";
+
+const app = express();
+const port = 3000
+
+// parser
+app.use(express.json());
+app.use(cors())
+
+app.use('/api/v1/students', StudentRoutes);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+})
+
+export default app;
